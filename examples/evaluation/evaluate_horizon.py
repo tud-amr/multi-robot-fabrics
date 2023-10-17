ac6ea8d7310dfdce7d150ce45dad45f891fbc245
Saray <saraybakker@gmail.com>
2023-10-17 12:37:22 +0200

    fixed panda_panda_rollouts. Changed naming of the files. Adapted pytests. evaluation scripts added again

--- examples/evaluation/evaluate_horizon.py
+++ examples/evaluation/evaluate_horizon.py
@@ -11,7 +11,6 @@ from examples.simulation_environments.create_simulation_manipulators import crea
 from multi_robot_fabrics.utils.utils import UtilsKinematics
 import examples.parameters_manipulators as parameters_manipulators
 
-
 def get_std(list_of_std: list) -> float:
     """
     input: list of standard deviations
